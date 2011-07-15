19edf9e9e7cac2a61b37f227e9118e594acf8b69
sanmugam <k.sanmugam2@gmail.com>
2011-07-15 19:37:07 +0530

    initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbel::Application.config.secret_token = '52b4398f8203cd61ff6bc2712f88984ed2e7cc9dceb056204192dc3b64521ee01211516753059043d80011f2af3d74c31543940b917e05ee672849dc7f1531c0'
